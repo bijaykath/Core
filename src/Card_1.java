3ba9c8d8fecec78f76509eee9406333cb1628301
bijay <bijaydhm@gmail.com>
2020-08-19 21:05:52 -0500

    I have added two files

---
+++ src/Card_1.java
@@ -0,0 +1,9 @@
+public class Card {
+
+    private String cardNumber;
+    private Float cardBalance;
+	//just comment added by Vishnu
+
+	private String userAddress;
+
+}
---
+++ src/Card_2.java
@@ -0,0 +1,9 @@
+public class Card_2 {
+
+    private String cardNumber;
+    private Float cardBalance;
+	//just comment added by Vishnu
+
+	private String userAddress;
+
+}
